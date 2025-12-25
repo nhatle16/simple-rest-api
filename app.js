c709864617e65a63f4c803d495345f332acbfa32
NolanL <nhatle.stu@gmail.com>
2025-12-25 16:13:12 -0600

    Implemented REST API logic and CRUD operations

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ require('dotenv').config({ path: './.env' });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 // Import data from data.js
 const courses = require('./data');
 
@@ -14,24 +16,108 @@ app.get('/', (req, res) => {
 });
 
 app.get('/courses', (req, res) => {
-
+    res.status(200).json({
+        success: true,
+        message: "Data is retrieved successfully",
+        data: courses
+    });
 });
 
 app.get('/courses/:courseID', (req, res) => {
+    const { courseID } = req.params;
+    const course = courses.find((course) => course.id === Number(courseID));
+    // Validate course search result
+    if (!course) {
+        return res.status(404).json({
+            success: false,
+            message: `No course with ID ${courseID}`
+        });
+    }
 
+    res.status(200).json({
+        success: true,
+        message: `Course ${courseID} is retrieved successfully`,
+        data: course
+    });
 });
 
 app.post('/courses', (req, res) => {
+    const newID = courses.length ? Math.max(...courses.map(c => c.id)) + 1 : 1;
+    const { name: courseName } = req.body;
+    // Validate request body
+    if (!courseName) {
+        return res.status(400).json({
+            success: false,
+            message: "courseName is required"
+        });
+    }
 
+    const newCourse = { id: newID, name: courseName };
+    courses.push(newCourse);
+    res.status(201).json({
+        success: true,
+        message: "Data is created successfully",
+        data: courses
+    });
 });
 
 app.put('/courses/:courseID', (req, res) => {
+    const { courseID } = req.params;
+    const { name: courseName } = req.body;
+    const course = courses.find((course) => course.id === Number(courseID));
+    // Validate course search
+    if (!course) {
+        return res.status(404).json({
+            success: false,
+            message: `No course with ID ${courseID}`
+        });
+    }
 
+    // Validate request body
+    if(!courseName) {
+        return res.status(400).json({
+            success: false,
+            message: "courseName is required"
+        });
+    }
+    course.name = courseName;
+    
+    res.status(200).json({
+        success: true,
+        message: "Data is updated successfully",
+        data: courses
+    });
 });
 
-app.patch('/courses/:courseID', (req, res) => {
+app.delete('/courses/:courseID', (req, res) => {
+    const { courseID } = req.params;
 
-})
+    // Validate the route parameter
+    if (Number.isNaN(courseID)) {
+        return res.status(400).json({
+            success: false,
+            message: "courseID must be a number"
+        });
+    }
+    
+    // Search for course
+    const course = courses.find((course) => course.id === Number(courseID));
+    if (!course) {
+        return res.status(404).json({
+            success: false,
+            message: `No course with ID ${courseID}`
+        });
+    }
+
+    // Remove the course with queried ID
+    const newCourses = courses.filter((course) => course.id !== Number(courseID));
+
+    return res.status(200).json({
+        success: true,
+        message: `Course ${courseID} is deleted successfully`,
+        data: newCourses
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
