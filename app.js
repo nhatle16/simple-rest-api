8479ee2db564a15af3da89395fb947c41fbccf46
NolanL <nhatle.stu@gmail.com>
2025-12-25 13:19:34 -0600

    Defined RESTful routes and added example data

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express');
-require('dotenv').config();
+require('dotenv').config({ path: './.env' });
 
 // Create an Express.js instance
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Import data from data.js
+const courses = require('./data');
+
 // Define a simple GET endpoint
 app.get('/', (req, res) => {
     res.send("<h1>WELCOME TO THE PAGE</h1>");
+});
+
+app.get('/courses', (req, res) => {
+
+});
+
+app.get('/courses/:courseID', (req, res) => {
+
+});
+
+app.post('/courses', (req, res) => {
+
+});
+
+app.put('/courses/:courseID', (req, res) => {
+
+});
+
+app.patch('/courses/:courseID', (req, res) => {
+
 })
 
 app.listen(PORT, () => {
---
+++ data.js
@@ -0,0 +1,9 @@
+const courses = [
+  { id: 1, name: "Introduction to Programming" },
+  { id: 2, name: "Web Development Basics" },
+  { id: 3, name: "Data Structures" },
+  { id: 4, name: "Databases 101" },
+  { id: 5, name: "Introduction to AI" }
+];
+
+module.exports = courses;
